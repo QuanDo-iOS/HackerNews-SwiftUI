cb63b0cdc9a3a8543f42002479a967de078d8876
MinhQuan-Github <minhquan.5322@aol.com>
2022-01-23 15:54:21 +0700

    create project

---
+++ Hacker News (swiftUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News (swiftUI)
+//
+//  Created by Quan's Macbook on 23/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hacker News (swiftUI)/Hacker_News__swiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News__swiftUI_App.swift
+//  Hacker News (swiftUI)
+//
+//  Created by Quan's Macbook on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News__swiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
